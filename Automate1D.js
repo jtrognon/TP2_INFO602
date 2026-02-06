cc157c00025e52cebf0d87ba3d15f3ca8cbb280a
lena <lena.bechet73@gmail.com>
2026-02-06 10:13:25 +0100

    automate1D

---
+++ Automate1D.js
@@ -0,0 +1,29 @@
+const automate1D = [];
+automate1D.length = 11;
+automate1D.fill("0");
+
+const regle = {
+    "111" : "0",
+    "110" : "1",
+    "101" : "1",
+    "100" : "0",
+    "011" : "1",
+    "010" : "1",
+    "001" : "1",
+    "000" : "0"
+}
+
+function initAutomate(automate1D) {
+    automate1D[Math.floor(automate1D.length/2)] = "1";
+}
+
+function next(automate1D){
+    const length = automate1D.length-1;
+    automate1D[0] = regle["0"+automate1D[0]+automate1D[1]];
+
+    for (let i=1; i<length; i++) {
+        automate1D[i] = regle[automate1D[i-1]+automate1D[i]+automate1D[i+1]];
+    }
+
+    automate1D[length] = regle[automate1D[length-1]+automate1D[length]+"0"];
+}
